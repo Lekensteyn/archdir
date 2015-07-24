01ec0b3062f8bf8ca9b18df58b10c367f3ea54b8
Peter Wu <peter@lekensteyn.nl>
2015-07-24 20:23:32 +0200

    build.sh: allow BUILD_OUTDIR to by specified
    Packages and logs end up in $BUILD_OUTDIR, buildrc can then change the
    working directory to build somewhere else.
    
    For instance:
    
        sudo rm -rf /build && sudo cp -ar . /build && cd /build

--- installer/build.sh
+++ installer/build.sh
@@ -23,18 +23,21 @@ if [ -f commands -a -x commands ]; then
     ./commands || error "command returned exit code $?"
 elif [ -e PKGBUILD ]; then
     sudo pacman-db-upgrade # Installed using pacman 4.1.2, runs 4.2
+    # Output logs and packages to this directory by default
+    BUILD_OUTDIR=$(pwd)
     if [ -f buildrc ]; then
         # can be used to load additional environment variables or more generally
         # invoke commands before running makepkg.
         . ./buildrc || error "buildrc returned exit code $?"
     fi
     # Hide stdout to avoid clogging journald. Use tail -F build.log instead
+    PKGDEST=$BUILD_OUTDIR \
     script -f -e -c \
         'time makepkg -s --noconfirm' \
-        build.log >/dev/null
+        "$BUILD_OUTDIR/build.log" >/dev/null
     rc=$?
-    pacman -Q > package-versions.txt
-    ls -l *.pkg.tar.*
+    pacman -Q > "$BUILD_OUTDIR/package-versions.txt"
+    ls -l "$BUILD_OUTDIR"/*.pkg.tar.*
     [ $rc -eq 0 ] || error "makepkg returned exit code $rc"
     [ -e .noshutdown ] || sudo shutdown -h now
 else
